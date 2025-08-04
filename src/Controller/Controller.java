da07b16c7ec2d246b300e91f98bbd63f90a97504
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2025-08-04 19:49:03 +0200

    aggiunto il package GUI

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,9 +1,15 @@
 package Controller;
 
+import GUI.FinestraLogin;
+
 public class Controller {
 
 	public static void main(String[] args) {
 		System.out.println("Ciao sono il controller");
+		FinestraLogin frame = new FinestraLogin();
+		frame.setVisible(true);
+
+
 	}
 
 }
