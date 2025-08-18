7c5c8eb82952f03c0560c8866668d4bbe5d1d055
eman-giaquinto <eman.giaquinto@studenti.unina.it>
2025-08-18 19:05:13 +0200

    Realizzato package Exception

---
+++ src/Exception/ExcpetionCampoPasswordVuoto.java
@@ -0,0 +1,9 @@
+package Exception;
+
+public class ExcpetionCampoPasswordVuoto extends Exception{
+	private String messaggioWarningSchermo = "Campo PASSWORD vuoto, inserisci un valore valido";
+
+	public String getmessaggioWarningSchermo() {
+		return messaggioWarningSchermo;
+	}
+}
---
+++ src/Exception/ExcpetionCampoUsernameVuoto.java
@@ -0,0 +1,9 @@
+package Exception;
+
+public class ExcpetionCampoUsernameVuoto extends Exception {
+	private String messaggioWarningSchermo = "Campo USERNAME vuoto, inserisci un valore valido";
+
+	public String getmessaggioWarningSchermo() {
+		return messaggioWarningSchermo;
+	}
+}
