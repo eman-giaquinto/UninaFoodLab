fca3190790cda73ff1d2d85774f5fdfc346efe3a
AlessandroFioretto <aless.fioretto@studenti.unina.it>
2025-09-17 16:43:27 +0200

    Realizzato ImplementazioneDAO di Ingrediente.

---
+++ src/ImplementazioniDAO/ImplementazioneIngredienteDAO.java
@@ -0,0 +1,9 @@
+package ImplementazioniDAO;
+
+import DAO.IngredienteDAO;
+
+public class ImplementazioneIngredienteDAO implements IngredienteDAO  {
+
+	//aggiungere la logica per il report mensile
+	
+}
