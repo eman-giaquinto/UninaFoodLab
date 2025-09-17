4e25a48028ccc7be3b953e60e2c5e54a1d163eac
AlessandroFioretto <aless.fioretto@studenti.unina.it>
2025-09-17 15:57:37 +0200

    Realizzato il DTO di Ingrediente.

---
+++ src/DTO/Ingrediente.java
@@ -0,0 +1,20 @@
+package DTO;
+
+public class Ingrediente {
+
+	private String nome;
+	
+	public Ingrediente(String nome)
+	{
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
